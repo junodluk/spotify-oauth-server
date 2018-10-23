4e0e3f519cdb42353a3479735b0cc949d0619d43
Alcione de Lucca Júnior (Juno) <junior.dluk@gmail.com>
2018-10-22 21:56:01 -0300

    Server flow

--- index.js
+++ index.js
@@ -51,3 +51,72 @@ const spotifyRequest = params => {
       });
     });
 };
+
+// Route to obtain a new Token
+app.post('/exchange', (req, res) => {
+ 
+  const params = req.body;
+  if (!params.code) {
+    return res.json({
+      "error": "Parameter missing"
+    });
+  }
+ 
+  spotifyRequest({
+      grant_type: "authorization_code",
+      redirect_uri: CLIENT_CALLBACK_URL,
+      code: params.code
+    })
+    .then(session => {
+      let result = {
+        "access_token": session.access_token,
+        "expires_in": session.expires_in,
+        "refresh_token": encrypt(session.refresh_token)
+      };
+      return res.send(result);
+    })
+    .catch(response => {
+      return res.json(response);
+    });
+});
+
+// Get a new access token from a refresh token
+app.post('/refresh', (req, res) => {
+  const params = req.body;
+  if (!params.refresh_token) {
+    return res.json({
+      "error": "Parameter missing"
+    });
+  }
+ 
+  spotifyRequest({
+      grant_type: "refresh_token",
+      refresh_token: decrypt(params.refresh_token)
+    })
+    .then(session => {
+      return res.send({
+          "access_token": session.access_token,
+          "expires_in": session.expires_in
+      });
+    })
+    .catch(response => {
+      return res.json(response);
+    });
+});
+
+// Helper functions
+function encrypt(text) {
+  return CryptoJS.AES.encrypt(text, ENCRYPTION_SECRET).toString();
+};
+ 
+function decrypt(text) {
+  var bytes = CryptoJS.AES.decrypt(text, ENCRYPTION_SECRET);
+  return bytes.toString(CryptoJS.enc.Utf8);
+};
+ 
+// Start the server
+var server = http.createServer(app);
+ 
+server.listen(process.env.PORT || 5000, function (err) {
+  console.info('listening in http://localhost:8080');
+});
